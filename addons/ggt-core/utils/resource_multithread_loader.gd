97ec019c17a109ba9128bc03acfdf5cfb3b8f09e
dusk <y.bera003.06@protonmail.com>
2024-11-12 01:19:22 +0300

    fix: use multithreaded loader properly so it works on web

--- addons/ggt-core/utils/resource_multithread_loader.gd
+++ addons/ggt-core/utils/resource_multithread_loader.gd
@@ -3,21 +3,15 @@ extends RefCounted
 signal resource_loaded(res)
 signal resource_stage_loaded(progress_percentage)
 
-const SIMULATED_DELAY_MS = 32
 
-var thread
 var stages_amount: int
 
 
 func load_resource(path):
-	if thread == null:
-		thread = Thread.new()
 	if ResourceLoader.has_cached(path):
 		return ResourceLoader.load(path)
 	else:
-		var state = thread.start(Callable(self, "_thread_load").bind(path))
-		if state != OK:
-			push_error("Error while starting thread: " + str(state))
+		_thread_load(path)
 
 
 func _thread_load(path):
@@ -42,17 +36,10 @@ func _thread_load(path):
 		else:
 			call_deferred("emit_signal", "resource_stage_loaded", float(progress_arr[0]))
 			pass
-		OS.delay_msec(SIMULATED_DELAY_MS)
+	
 	call_deferred("_thread_done", res)
 
 
 func _thread_done(resource):
 	assert(resource)
-	# Always wait for threads to finish, this is required on Windows.
-	thread.wait_to_finish()
-	emit_signal("resource_loaded", resource)
-
-
-#func _exit_tree():
-#	if thread and thread.is_alive():
-#		thread.wait_to_finish()
+	resource_loaded.emit(resource)
