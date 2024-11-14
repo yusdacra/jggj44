adcda974b5e0cfe59f3fd3eabf84bd11867bd298
dusk <y.bera003.06@protonmail.com>
2024-11-14 22:38:10 +0300

    feat: add small bg to interact text

--- scenes/gameplay/ui-layer/ui_layer.gd
+++ scenes/gameplay/ui-layer/ui_layer.gd
@@ -5,8 +5,10 @@ class_name UiLayer
 
 var target_interact_opacity := 0.0
 var target_overlay_opacity := 0.0
+var hint_text_length := 0
 
 func _ready() -> void:
+	hint_text_length = %InteractHint/Panel/Text.text.length()
 	%InteractHint.modulate.a = 0.0
 	%Overlay.self_modulate.a = 0.0
 
@@ -17,7 +19,10 @@ func _process(delta: float) -> void:
 	%Overlay.self_modulate.a = move_toward(%Overlay.self_modulate.a, target_overlay_opacity, modulate_by)
 
 func show_interact_text(text: String) -> void:
-	%InteractHint/Text.text = text
+	%InteractHint/Panel/Text.text = ""
+	for i in (hint_text_length - text.length() - 1):
+		%InteractHint/Panel/Text.text += " "
+	%InteractHint/Panel/Text.text += text
 	target_interact_opacity = 1.0
 
 func hide_interact_text() -> void:
